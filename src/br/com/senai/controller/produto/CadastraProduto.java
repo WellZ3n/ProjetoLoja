543d3d98fc08e595220d84799cba25f12b909cb1
wellz3n <tomwudkeww@gmail.com>
2021-05-11 16:52:08 -0300

    Cadastro de dados no banco

--- src/br/com/senai/controller/produto/CadastraProduto.java
+++ src/br/com/senai/controller/produto/CadastraProduto.java
@@ -1,13 +1,22 @@
 package br.com.senai.controller.produto;
 
+import java.sql.Connection;
 import java.util.Scanner;
 
+import java.sql.PreparedStatement;
+
+import br.com.dao.DataBaseConnection;
 import br.com.senai.model.ProdutoModel;
 
 public class CadastraProduto {
 	
 	Scanner entrada = new Scanner(System.in);
 	ProdutoModel produtoModel;
+	private Connection connection;
+	
+	public CadastraProduto() {
+		connection = DataBaseConnection.getInstance().getConnection();
+	}
 
 	public ProdutoModel cadastrarProduto() {
 		produtoModel = new ProdutoModel();
@@ -21,6 +30,21 @@ public class CadastraProduto {
 		produtoModel.setQuantidadeDeProduto(entrada.nextInt());
 		produtoModel.setSaldoEmEstoque(produtoModel.getQuantidadeDeProduto() * produtoModel.getPrecoDoProduto());
 
+		try {
+			String sql = "insert into produto (nomeDoProduto, precoDoProduto, quantidadeDeProduto, saldoEmEstoque) values (?, ?, ?, ?)";
+			PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+			preparedStatement.setString(1, produtoModel.getNomeDoProduto());
+			preparedStatement.setDouble(2, produtoModel.getPrecoDoProduto());
+			preparedStatement.setInt(3, produtoModel.getQuantidadeDeProduto());
+			preparedStatement.setDouble(4, produtoModel.getSaldoEmEstoque());
+			
+			preparedStatement.execute();
+			
+		} catch (Exception e){
+			System.out.println("<-< Erro ao cadastrar os dados >->");
+		}
+		
 		return produtoModel;
 	}
 }
